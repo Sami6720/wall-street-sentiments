ffa98dd983e43c58b2f18ee8e1c5ad5c954e9dd6
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-05-27 17:10:57 -0400

    feat(config): add .env.example, update config file, and update .gitignore
    - Added .env.example file to serve as a template for environment variables
    - Modified the config file to accommodate new environment variables
    - Updated .gitignore to exclude the .env file

--- experimentation/notebooks/config.py
+++ experimentation/notebooks/config.py
@@ -1,27 +1,12 @@
+import os
 import yaml
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class Config:
     """Config class"""
 
     def __init__(self):
-        # Change this to your config.yaml file path
-        CONFIG_FILE_PATH = "..\..\common\config\config.yaml"
-        self.load_config(config_file_path=CONFIG_FILE_PATH)
-
-    def load_config(self, config_file_path: str) -> None:
-        """Load config from config.yaml file in common/config folder
-        
-        param config_file_path: path to config.yaml file
-        type: str
-        """
-        with open(config_file_path, "r", encoding='utf-8') as file:
-            config = yaml.safe_load(file)
-
-            # Reddit API config
-            self.reddit_api_client = config["reddit_api_config"]["client_id"]
-            self.reddit_api_secret = config["reddit_api_config"]["client_secret"]
-            self.reddit_api_user_agent = config["reddit_api_config"]["user_agent"]
-
-            # FinnHub API config
-            self.finnhub_api_key = config["finnhub_api_config"]["api_key"]
+        self.finnhub_api_key = os.getenv("FINHUB_API_KEY")
+        self.bucket_name = os.getenv("BUCKET_NAME")
