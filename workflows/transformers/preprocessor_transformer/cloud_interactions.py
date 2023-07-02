eee13f67701b599debdb8d3bd617248de6f86b3b
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-07-01 23:16:05 -0400

    feat(transformer): Add build_s3_path function to preprocessor_transformer.

--- workflows/transformers/preprocessor_transformer/cloud_interactions.py
+++ workflows/transformers/preprocessor_transformer/cloud_interactions.py
@@ -3,6 +3,24 @@ import botocore
 import pandas as pd
 
 
+def build_s3_path(data_path_prefix: str,
+                  today: str, data_type: str) -> str:
+    """Build S3 path
+
+    param bucket_name: Name of S3 bucket.
+    type: str
+    param data_path_prefix: Path prefix to data.
+    type: str
+    param today: Today's date.
+    type: str
+    param data_type: Type of data.
+    type: str
+
+    return: S3 path.
+    rtype: str
+    """
+    return f's3://{data_path_prefix}/{data_type}_{today}.csv'
+
 def get_data_from_s3(bucket_name: str, data_path_prefix: str,
                      today: str, data_type: str) -> pd.DataFrame:
     """Get data from S3
--- workflows/transformers/preprocessor_transformer/lambda_function.py
+++ workflows/transformers/preprocessor_transformer/lambda_function.py
@@ -4,7 +4,8 @@ import pytz
 from preprocessor_transformer import create_new_feature_columns, \
     impute_values_in_columns, \
     rename_columns
-from cloud_interactions import get_data_from_s3, upload_data_to_s3
+from cloud_interactions import get_data_from_s3, upload_data_to_s3, \
+    build_s3_path
 
 config = Config()
 BUCKET_NAME = config.bucket_name
@@ -27,11 +28,15 @@ def lambda_handler(event, context) -> None:
     current_time = datetime.now(timezone)
     today = current_time.strftime('%m-%d-%Y')
 
+    top_stocks_info_extracted_path = build_s3_path(
+        TOP_STOCKS_INFO_EXTRACTED_PATH_PREFIX, today, 'top_stocks_info')
     top_stocks_info_extracted = get_data_from_s3(
         BUCKET_NAME, TOP_STOCKS_INFO_EXTRACTED_PATH_PREFIX, today, 'top_stocks_info')
     preprocessed_data = rename_columns(top_stocks_info_extracted)
     preprocessed_data = create_new_feature_columns(preprocessed_data)
     preprocessed_data = impute_values_in_columns(preprocessed_data)
 
+    preprocessed_data_path = build_s3_path(
+        PREPROCESSED_DESTINATION_PATH_PREFIX, today, 'preprocessed_data')
     upload_data_to_s3(BUCKET_NAME, PREPROCESSED_DESTINATION_PATH_PREFIX,
                       preprocessed_data, today, 'preprocessed_data')
