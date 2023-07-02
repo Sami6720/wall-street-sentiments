e19a2cc85421cbc6370d80b22117e0a6a058e2a5
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-07-02 00:03:57 -0400

    refactor(transformer): Update lambda_function to use new functions from cloud_interactions for labelling_transformer.

--- workflows/transformers/labelling_transformer/lambda_function.py
+++ workflows/transformers/labelling_transformer/lambda_function.py
@@ -3,7 +3,8 @@ import boto3
 from config import Config
 import pytz
 from labelling_transformer import label_data
-from cloud_interactions import get_data_from_s3, upload_data_to_s3
+from cloud_interactions import get_data_from_s3, upload_data_to_s3, \
+    build_s3_path
 
 config = Config()
 BUCKET_NAME = config.bucket_name
@@ -27,12 +28,12 @@ def lambda_handler(event, context) -> None:
     preprocessed_data_path = event['transformers']['preprocessorTransformer']['pathPreprocessedData']
     top_stocks_prices_extracted_path = event['extractors']['topStocksPricesExtractor']['topStockPricesPath']
 
-    preprocessed_data = get_data_from_s3(BUCKET_NAME, PREPROCESSED_DATA_PATH_PREFIX,
-                                         today, 'preprocessed_data')
-    top_stocks_prices_extracted = get_data_from_s3(BUCKET_NAME, TOP_STOCKS_PRICES_EXTRACTED_PREFIX,
-                                                   today, 'top_stocks_opening_closing_prices')
+    preprocessed_data = get_data_from_s3(BUCKET_NAME, preprocessed_data_path)
+    top_stocks_prices_extracted = get_data_from_s3(
+        BUCKET_NAME, top_stocks_prices_extracted_path)
     labelled_data = label_data(
         preprocessed_data, top_stocks_prices_extracted)
 
-    upload_data_to_s3(BUCKET_NAME, LABELLED_DATA_DESTINATION_PREFIX,
-                      labelled_data, today, 'labelled_data')
+    labelled_data_path = build_s3_path(
+        LABELLED_DATA_DESTINATION_PREFIX, today, 'labelled_data')
+    upload_data_to_s3(BUCKET_NAME, labelled_data_path, labelled_data)
