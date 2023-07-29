cf2c5c6f7b660b9c229191f7a7fb1bf828832b5d
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-07-29 17:16:11 -0400

    feat(preprocessor): Add the storing of preprocessed data as json to s3.
    - This will be used to load stock metrics.

--- workflows/transformers/preprocessor_transformer/cloud_interactions.py
+++ workflows/transformers/preprocessor_transformer/cloud_interactions.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 
 def build_s3_path(data_path_prefix: str,
+                  file_extension: str,
                   today: str, data_type: str) -> str:
     """Build S3 path
 
     param bucket_name: Name of S3 bucket.
+    type: str
+    param extension: Extension of file.
     type: str
     param data_path_prefix: Path prefix to data.
     type: str
@@ -19,7 +22,7 @@ def build_s3_path(data_path_prefix: str,
     return: S3 path.
     rtype: str
     """
-    return f'{data_path_prefix}/{data_type}_{today}.csv'
+    return f'{data_path_prefix}/{data_type}_{today}.{file_extension}'
 
 
 def get_data_from_s3(bucket_name: str, s3_path: str) -> pd.DataFrame:
@@ -43,7 +46,7 @@ def get_data_from_s3(bucket_name: str, s3_path: str) -> pd.DataFrame:
         raise error
 
 
-def upload_data_to_s3(bucket_name: str, s3_path: str, data: pd.DataFrame) -> None:
+def upload_data_to_s3(bucket_name: str, s3_path: str, data: any) -> None:
     """Upload data to S3
 
     param bucket_name: Name of S3 bucket.
@@ -51,7 +54,7 @@ def upload_data_to_s3(bucket_name: str, s3_path: str, data: pd.DataFrame) -> Non
     param s3_path: Path to data.
     type: str
     param data: Data to be uploaded.
-    type: pd.DataFrame
+    type: csv or json
 
     return: None
     """
@@ -59,6 +62,6 @@ def upload_data_to_s3(bucket_name: str, s3_path: str, data: pd.DataFrame) -> Non
     s3 = boto3.client('s3')
     try:
         s3.put_object(Bucket=bucket_name, Key=s3_path,
-                      Body=data.to_csv(index=False))
+                      Body=data)
     except botocore.exceptions.ClientError as error:
         raise error
