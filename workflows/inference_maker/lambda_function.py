a5cff9520f56e359919e89bc79dbfa1ddf4891b0
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-07-30 12:22:53 -0400

    refactor(inference_maker): Update the lambda_function with the following changes:
    - Add the ability to load the model from S3. The previous loading mechanism was buggy.
    - Make use of new combine_predictions_and_preprocessed_data function.
    - Make use of get_data_from_s3 function.
    - Make code more readable.

--- workflows/inference_maker/lambda_function.py
+++ workflows/inference_maker/lambda_function.py
@@ -1,8 +1,10 @@
-from inference_maker import add_meta_data
+from io import BytesIO
+from inference_maker import combine_predictions_and_preprocessed_data
 from cloud_interactions import get_data_from_s3, upload_data_to_s3, build_s3_path
 from config import Config
 from logger import logger
 import joblib
+import pandas as pd
 
 config = Config()
 
@@ -14,22 +16,20 @@ def lambda_handler(event, context):
     logger.info("Starting inference maker workflow")
 
     workflow_date = event['workflowStartDate']
-    preproceessed_data_path = event['pathPreprocessedData']
+    preproceessed_data_path = event['preprocessedDataPath']
 
-    preprocessed_data = get_data_from_s3(config.bucket_name, preproceessed_data_path)
+    preprocessed_data = pd.read_csv(get_data_from_s3(config.bucket_name, preproceessed_data_path))
 
     xgboost_model_path = config.xgboost_model_path
     random_forest_model_path = config.random_forest_model_path
 
-    xgboost_model = joblib.load(get_data_from_s3(config.bucket_name, xgboost_model_path))
-    random_forest_model = joblib.load(get_data_from_s3(config.bucket_name, random_forest_model_path))
+    xgboost_model = joblib.load(BytesIO(get_data_from_s3(config.bucket_name, xgboost_model_path).read()))
+    random_forest_model = joblib.load(BytesIO(get_data_from_s3(config.bucket_name, random_forest_model_path).read()))
 
-    preprocessed_data = preprocessed_data.drop(
+    feature_engineered_data = preprocessed_data.drop(
         ['name',
          'ticker',
          'timestamp',
-         'opening_price',
-         'closing_price',
          'rank_24h_ago',
          'mentions_24h_ago',
          'rank', 'dividend_exists'
@@ -37,15 +37,20 @@ def lambda_handler(event, context):
         by=['mentions'],
         ascending=False)
 
-    xgboost_predictions = xgboost_model.predict(preprocessed_data)
-    random_forest_predictions = random_forest_model.predict(preprocessed_data)
+    xgboost_predictions = xgboost_model.predict(feature_engineered_data)
+    random_forest_predictions = random_forest_model.predict(feature_engineered_data)
 
-    xgboost_predictions = add_meta_data(xgboost_predictions, preprocessed_data, 'xgboost')
-    random_forest_predictions = add_meta_data(random_forest_predictions, preprocessed_data, 'random_forest')
+    xgboost_predictions = combine_predictions_and_preprocessed_data(
+        xgboost_predictions, preprocessed_data.copy(), 'xgboost')
+    random_forest_predictions = combine_predictions_and_preprocessed_data(
+        random_forest_predictions, preprocessed_data.copy(), 'random_forest')
 
-    xgboost_predictions_path = build_s3_path(config.predictions_destination_prefix, 'csv', workflow_date, 'xgboost')
+    xgboost_predictions_path = build_s3_path(
+        config.predictions_destination_prefix, 'csv', workflow_date, 'xgboost'
+    )
     random_forest_predictions_path = build_s3_path(
-        config.predictions_destination_prefix, 'csv', workflow_date, 'random_forest')
+        config.predictions_destination_prefix, 'csv', workflow_date, 'random_forest'
+    )
 
     upload_data_to_s3(config.bucket_name, xgboost_predictions_path, xgboost_predictions)
     upload_data_to_s3(config.bucket_name, random_forest_predictions_path, random_forest_predictions)
