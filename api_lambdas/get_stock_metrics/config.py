f81b877bd2129ce525f1265b4e8555e4c6ec26a7
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-08-06 12:43:01 -0400

    feat(api_lambdas): Create get_predictions and get_stock_metrics lambdas.

---
+++ api_lambdas/get_stock_metrics/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config():
+    """
+    Class to store configuration variables.
+    """
+
+    def __init__(self) -> None:
+        self.db_connection_string = os.environ['DB_CONNECTION_STRING']
+        self.db_name = os.environ['DB_NAME']
+        self.collection_name = os.environ['STOCK_METRICS_COLLECTION_NAME']
