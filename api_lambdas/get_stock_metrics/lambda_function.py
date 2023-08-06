24290987146725178349db029936ec5871cbe44b
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-08-06 17:35:38 -0400

    feat(api_lambdas): Add CORS header to get_stock_metrics lambda.

--- api_lambdas/get_stock_metrics/lambda_function.py
+++ api_lambdas/get_stock_metrics/lambda_function.py
@@ -61,7 +61,8 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
         'body': json_util.dumps(stock_metrics_dict)
     }
